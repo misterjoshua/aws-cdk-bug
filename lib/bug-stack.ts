496cb1e7c21b81bcae9b11ac2d9c14fabea42ac0
Josh Kellendonk <joshkellendonk@gmail.com>
2020-06-26 10:40:43 -0600

    Commit

--- lib/bug-stack.ts
+++ lib/bug-stack.ts
@@ -1,8 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { CfnOutput } from '@aws-cdk/core';
 
 export class BugStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const fn = new lambda.Function(this, 'fn', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('./lib'),
+      handler: 'bug-handler.handler',
+    });
+
+    new CfnOutput(this, 'output', {
+      value: fn.functionName,
+    });
 
     // The code that defines your stack goes here
   }
